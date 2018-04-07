f6b4b6a62b68c7592b623ffca060720748cad2e1
k----n <kalvin@kalvineng.com>
2018-04-07 10:36:16 -0600

    Extracted all the tables and contents from articles

--- wikitables/__init__.py
+++ wikitables/__init__.py
@@ -25,21 +25,24 @@ def import_tables(article, lang="en"):
     ## get sections
     sections = parsed_body.get_sections(include_lead = False, include_headings = True, flat = True)
 
+    print(page['title'])
     section_count = 0
     for idx, s in enumerate(sections):
         t = s.filter_tags(matches=ftag('table'))
         if t:
-            head = mwp.parse(s.filter_headings()[0]).strip_code()
-            tables_info[section_count]["head"] = head
+            head = mwp.parse(s.filter_headings()[0])
+            tables_info[section_count]["head"] = head.strip_code()
             s.remove(head)
             table_count = 0
             for i,x in enumerate(t):
-                name = '{}|Table {}'.format(page['title'],table_count)
-                wt = WikiTable(name, x)
-                if not wt.flag:
-                    tables_info[section_count]["table"][table_count] = wt
-                    table_count+=1
-                s.remove(x)
+                nested = mwp.parse(x).filter_tags(matches=ftag('table'))
+                if not nested:
+                    name = '{}|Table {}'.format(page['title'],table_count)
+                    wt = WikiTable(name, x)
+                    if not wt.flag:
+                        tables_info[section_count]["table"][table_count] = wt
+                        table_count+=1
+                    s.remove(x)
             tables_info[section_count]["text"] = s.strip_code()
             section_count+=1
 
