b09e0bef407bf353b29aa86f3a5d97e2a4f726f8
k----n <kalvin@kalvineng.com>
2018-04-08 10:30:11 -0600

    Wikidata feature helper functions

--- wikitables/client.py
+++ wikitables/client.py
@@ -13,6 +13,26 @@ class Client(requests.Session):
         super(Client, self).__init__()
         self.base_url = 'https://' + lang + '.wikipedia.org/w/api.php'
 
+    def fetch_wikidata(self, title, method='GET'):
+        """ Query for page by wikidata entity """
+        params = { 'prop': 'pageprops',
+                   'format': 'json',
+                   'action': 'query',
+                   'titles': title,
+                   'redirects': 1,
+                   'ppprop': 'wikibase_item' }    
+
+        r = self.request(method, self.base_url, params=params)
+        r.raise_for_status()
+        pages = r.json()["query"]["pages"]
+        # use key from first result in 'pages' array
+        pageid = list(pages.keys())[0]
+        if pageid == '-1':
+            raise ArticleNotFound('no matching articles returned') 
+
+        return pages[pageid]["pageprops"]["wikibase_item"]
+    
+
     def fetch_page(self, title, method='GET'):
         """ Query for page by title """
         params = { 'prop': 'revisions',
